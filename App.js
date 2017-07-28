98811b5277030f94bc09d5ce771c2b3135b6fb28
AdamMc331 <amcneilly331@gmail.com>
2017-07-27 23:48:49 -0400

    Initial commit.

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello, world!</Text>
+        <Text>Your friendly neighborhood stubborn Android dev here.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
